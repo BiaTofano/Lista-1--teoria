675929998c1de5b9174ad2914e7d2e2cd8e1f87e
BiaTofano <beatriz.t.lemos@gmail.com>
2024-09-09 18:17:53 -0300

    Add files via upload

---
+++ 2-b.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+    char nome [1][100];
+
+    printf("Ola! Eu sou o numero 1, como é o seu nome?\n");
+    gets(nome);
+    printf("Bem vindo ao clube %s", nome);
+
+return 0;
+
+}
