a7f8aed14284bbbe7f527c30b375079a1ab0e127
BiaTofano <beatriz.t.lemos@gmail.com>
2024-09-09 18:09:35 -0300

    Add files via upload

---
+++ 2-a.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+ int a;
+ int b;
+ int c;
+
+ printf("Digite três numeros inteiros: \n");
+
+ scanf("%d", &a);
+ scanf("%d", &b);
+ scanf("%d", &c);
+
+ printf("\n%d\n", c);
+ printf("%d\n",b);
+ printf("%d\n",a);
+
+ return 0;
+
+}
